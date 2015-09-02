9c7d3d3f3d5629738bebfb6657ef79931a413729
Joshua Burgin <jsburgin@crimson.ua.edu>
2015-09-01 22:45:58 -0500

    updated for herkou

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 
 require('dotenv').load();
 
-var connection = process.env.CONNECTION_STRING
+var connection = process.env.DATABASE_URL
 
 exports.runQuery = function(query, next) {
 	pg.connect(connection, function(err, client, done) {
